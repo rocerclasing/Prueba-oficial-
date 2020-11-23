60c1250de1dc373a22548be97e049859d7e4ce0f
Robert <robertclasing@hotmail.com>
2020-11-22 21:49:13 -0300

    version 1

---
+++ app.js
@@ -0,0 +1,420 @@
+let auxiliar;
+const clients = [{
+    id: 1,
+    taxNumber: '86620855',
+    name: 'HECTOR ACUÑA BOLAÑOS'
+},
+{
+    id: 2,
+    taxNumber: '7317855K',
+    name: 'JESUS RODRIGUEZ ALVAREZ'
+},
+{
+    id: 3,
+    taxNumber: '73826497',
+    name: 'ANDRES NADAL MOLINA'
+},
+{
+    id: 4,
+    taxNumber: '88587715',
+    name: 'SALVADOR ARNEDO MANRIQUEZ'
+},
+{
+    id: 5,
+    taxNumber: '94020190',
+    name: 'VICTOR MANUEL ROJAS LUCAS'
+},
+{
+    id: 6,
+    taxNumber: '99804238',
+    name: 'MOHAMED FERRE SAMPER'
+},
+{
+    id: 7,
+    taxNumber: '92121901',
+    name: 'Flabio Contreras '
+
+}
+];
+
+//respuesta pregunta 2 y 8,0,1
+
+listClientsIds = new listClientsIds[{
+    id:'1 , 2 ,3 , 4 , 5 , 6,7',
+    taxNumber: '86620855',
+    taxNumber: '7317855K',
+    taxNumber: '73826497',
+    taxNumber: '88587715',
+    taxNumber: '94020190',
+    taxNumber: '99804238',
+    taxNumber: '92121901',
+    idname: 'HECTOR ACUÑA BOLAÑOS',
+    name: 'JESUS RODRIGUEZ ALVAREZ',
+    name: 'ANDRES NADAL MOLINA',
+    name: 'SALVADOR ARNEDO MANRIQUEZ',
+    name: 'VICTOR MANUEL ROJAS LUCAS',
+    name: 'MOHAMED FERRE SAMPER',
+    name: 'Flabio Contreras '
+    
+}];
+
+
+console.log(listClientsIds.SortByTaxNumber());
+
+//2 y 8
+const clientstotalbalance = new client[{
+    balance: 15000,
+    balance: 18000,
+    balance: 135000,
+    balance: 5600,
+    balance: 23000,
+    balance: 15000,
+    balance: 45900,
+    balance: 19000,
+    balance: 51000,
+    balance: 89000,
+    balance: 1600,
+    balance: 37500,
+    balance: 19200,
+    balance: 10000,
+    balance: 5400,
+    balance: 9000,
+    balance: 13500,
+    balance: 38200,
+    balance: 17000,
+    balance: 1000,
+    balance: 600,
+    balance: 16200,
+    balance: 10000,
+    balance:9000,
+    name: 'HECTOR ACUÑA BOLAÑOS',
+    name: 'JESUS RODRIGUEZ ALVAREZ',
+    name: 'ANDRES NADAL MOLINA',
+    name: 'SALVADOR ARNEDO MANRIQUEZ',
+    name: 'VICTOR MANUEL ROJAS LUCAS',
+    name: 'MOHAMED FERRE SAMPER',
+    name: 'Flabio Contreras '
+
+}];
+
+console.log(sortclientstotalbalance(name));
+
+var clave= {
+  name:'SANTANDER',
+  name:'CHile',
+  name:'Estado'
+}
+
+//esta es la respuesta 6
+var banksFidelity =
+{
+    clientId: 6,
+    name: 'SANTANDER',
+    clientId: 1,
+    name: 'ESTADO',
+    clientId: 5,
+    name: 'ESTADO', 
+    clientId: 2,
+    name: 'CHILE',
+    clientId: 3,
+    name: 'SANTANDER',
+    clientId: 5,
+    name: 'CHILE',
+    clientId: 3,
+    name: 'Estado',
+    clientId: 2,
+    name:'Estado',
+    clientId: 4,
+    name:'Estado',
+    clientId: 5,
+    name:'SANTANDER',
+    clientId: 1,
+    name:'CHile',
+    clientId: 5,
+    name:'Estado',
+    clientId: 6,
+    name:'SANTANDER',
+    clientId: 2,
+    name:'CHile',
+    clientId: 3,
+    name: 'Estado',
+    clientId: 3,
+    name: 'Chile',
+    clientId: 4,
+    name: 'SANTANDER',
+    clientId: 2,
+    name: 'CHILE',
+    clientId:5,
+    name: 'CHILE',
+    clientId: 6,
+    name:"Santiago",
+    clientId: 2,
+    name: 'CHILE',
+    clientId:3,
+    name:'Estado'
+
+
+
+
+
+    
+
+
+
+
+}
+
+//esta es la respuesta 7
+const banksPoorClients = {
+
+     
+    clientId: 6,
+    bankId: 1,
+    name: 'SANTANDER',
+    balance: 10000,
+    clientId: 1,
+    bankId: 3,
+    name: 'ESTADO',
+    balance: 15000,
+    clientId: 5,
+    bankId: 3,
+    name: 'ESTADO',
+    balance: 13000,
+    clientId: 2,
+    bankId: 2,
+    name: 'CHILE',
+    balance:5000,
+    clientId: 3,
+    bankId: 1,
+    name: 'SANTANDER',
+    balance: 20000,
+    clientId: 5,
+    bankId: 2,
+    name: 'CHILE',
+    balance:10000,
+    clientId: 3,
+    bankId: 3,
+    name: 'ESTADO',
+    balance: 40000,
+    clientId: 2,
+    bankId: 3,
+    name: 'ESTADO',
+    balance: 15000,
+    clientId: 4,
+    bankId: 3,
+    name: 'ESTADO',
+    balance:15000,
+    clientId: 5,
+    bankId: 1,
+    name: 'SANTANDER',
+    balance: 35900,
+    clientId: 1,
+    bankId: 2,
+    name: 'CHILE',
+    balance: 59000,
+    clientId: 5,
+    bankId: 3,
+    name: 'Estado',
+    balance: 1200,
+    clientId: 6,
+    bankId: 1,
+    name: 'SANTANDER',
+    balance: 30500,
+    clientId: 2,
+    name: 'CHile',
+    bankId: 3,
+    balance: 10200,
+    clientId: 3,
+    name: 'Estado',
+    bankId: 2,
+    balance: 5000,
+    clientId: 3,
+    name: 'Chile',
+    bankId: 1,
+    balance: 4400,
+    clientId: 4,
+    name: 'SANTANDER',
+    bankId: 3,
+    balance: 4400,
+    clientId: 2,
+    name: 'CHILE',
+    bankId: 1,
+    balance: 31200,
+    clientId: 5,
+    bankId: 2,
+    name: 'CHILE',
+    balance:12000,
+    clientId: 1,
+    bankId: 3,
+    name: 'Estado',
+    balance:200,
+    clientId: 5,
+    bankId: 2,
+    name: 'CHILE',
+    balance:100,
+    clientId: 6,
+    bankId: 1,
+    name: 'Santander',
+    balance:10000,
+    clientId: 2,
+    name: 'CHILE',
+    bankId: 2,
+    balance:5000,
+    clientId:3,
+    name:'Estado',
+    balance:9000,
+    bankid:3
+
+}
+
+
+
+const banksClients = [
+    {
+        taxNumber: '86620855',
+        taxNumber: '7317855K',
+        taxNumber: '73826497',
+        taxNumber: '88587715',
+        taxNumber: '94020190',
+        taxNumber: '94020190',
+        taxNumber: '99804238',
+        name: 'SANTANDER',
+        name: 'ESTADO',
+        name: 'CHile',
+        name: 'HECTOR ACUÑA BOLAÑOS',
+        name: 'JESUS RODRIGUEZ ALVAREZ',
+        name: 'ANDRES NADAL MOLINA',
+        name: 'SALVADOR ARNEDO MANRIQUEZ',
+        name: 'VICTOR MANUEL ROJAS LUCAS',
+        name: 'MOHAMED FERRE SAMPER',
+        name: 'Flabio Contreras '
+
+
+
+
+    }
+];
+
+console.log(banksClients.TaxNumbers());
+
+//pregunta 4 
+var richClients = [{
+    balance: 15000,
+    balance: 18000,
+    balance: 135000,
+    balance: 5600,
+    balance: 23000,
+    balance: 15000,
+    balance: 45900,
+    balance: 19000,
+    balance: 51000,
+    balance: 89000,
+    balance: 1600,
+    balance: 37500,
+    balance: 19200,
+    balance: 10000,
+    balance: 5400,
+    balance: 9000,
+    balance: 13500,
+    balance: 38200,
+    balance: 17000,
+    balance: 1000,
+    balance: 600,
+    balance: 16200,
+    balance: 10000,
+    balance:9000,
+    name: 'SANTANDER',
+    name: 'CHILE',
+    name: 'ESTADO'
+}]
+
+for( i=0; i<richClients.balance-1;i++)
+{
+
+    
+    if( richClients[i] > richClients[i+1] || richClients[i].balance >=25000)
+    {
+        
+        console.log(richClients.balance('pertenece al Santander'));
+    }
+
+
+}
+
+const banks = [{
+    id: 1,
+    name: 'SANTANDER'
+},
+{
+    id: 2,
+    name: 'CHILE'
+},
+{
+    id: 3,
+    name: 'ESTADO'
+}
+];
+
+//esta es la resúesta 5
+const banksRankingByTotalBalance = [{
+    bankId: 1,
+    balance: 15000,
+    bankId: 3,
+    balance: 18000,
+    bankId: 3,
+    balance: 135000,
+    bankId: 2,
+    balance: 5600,
+    bankId: 1,
+    balance: 23000,
+    bankId: 2,
+    balance: 15000,
+    bankId: 3,
+    balance: 45900,
+    bankId: 3,
+    balance: 19000,
+    bankId: 3,
+    balance: 51000,
+    bankId: 1,
+    balance: 89000,
+    bankId: 2,
+    balance: 1600,
+    bankId: 3,
+    balance: 37500,
+    bankId: 1,
+    balance: 19200,
+    bankId: 3,
+    balance: 10000,
+    bankId: 2,
+    balance: 5400,
+    bankId: 1,
+    balance: 9000,
+    bankId: 3,
+    balance: 13500,
+    bankId: 1,
+    balance: 38200,
+    bankId: 2,
+    balance: 17000,
+    bankId: 3,
+    balance: 1000,
+    bankId: 2,
+    balance: 600,
+    bankId: 1,
+    balance: 16200,
+    bankId: 2,
+    balance: 10000,
+    bankid:3,
+    balnce:5000
+
+
+}];
+
+for( j=0; j< banksRankingByTotalBalance-1; j++)
+{
+    if( banksRankingByTotalBalance[j] <  banksRankingByTotalBalance[j] + 1)
+    {
+        console.log(banksRankingByTotalBalance());
+    }
+
+}
+
